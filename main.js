b2392f60e61241ff4645064fc8be2a82752f4ae3
DaniieLizcano <dbarrios30@gmail.com>
2021-03-29 19:17:47 -0500

    29_03_21

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+// Codigo en funcion
+function Saludar(p1){
+    console.log(`Hola ${p1}`);
+    return null;
+}
+
+Saludar("Miguel Angel");
+Saludar("Marly");
+Saludar("Orlando");
+Saludar("Fabian");
+
+
+// Codigo ens ecuencia
+let saludar;
+saludar = "Miguel Angel";
+console.warn(`Hola ${saludar}`);
+saludar = "Marly";
+console.warn(`Hola ${saludar}`);
+saludar = "Orlando";
+console.warn(`Hola ${saludar}`);
+saludar = "Fabian";
+console.warn(`Hola ${saludar}`);
